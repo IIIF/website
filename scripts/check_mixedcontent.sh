79253f7c587f50cae8d38e3d01da5a989ad354bf
Glen Robson <glen.robson@gmail.com>
2018-07-04 16:02:37 +0100

    Simplified call to try and find error...

--- scripts/check_mixedcontent.sh
+++ scripts/check_mixedcontent.sh
@@ -1,17 +1,6 @@
 #!/bin/bash
 
 
-#mkdir files
-#cd files
-#curl -O https://raw.githubusercontent.com/acdha/phantomjs-mixed-content-scan/master/report-mixed-content.js
-cd files
-phantomjs --help
-chmod 755 ./report-mixed-content.js
 echo "Running mixed content report"
-pwd
-ls ../_site/test/
-./report-mixed-content.js ../_site/test/index.html
+phantomjs --debug=true files/report-mixed-content.js _site/test/index.html
 echo "report finsihed."
-rm report-mixed-content.js
-cd ..
-rmdir files
