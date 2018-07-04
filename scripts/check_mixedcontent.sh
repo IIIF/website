3acf8012c05786aec81bcb1fb62cac5ecb284644
Glen Robson <glen.robson@gmail.com>
2018-07-04 14:49:15 +0100

    Adding reporting echos

--- scripts/check_mixedcontent.sh
+++ scripts/check_mixedcontent.sh
@@ -6,9 +6,9 @@ cd files
 curl -O https://raw.githubusercontent.com/acdha/phantomjs-mixed-content-scan/master/report-mixed-content.js
 
 chmod 755 report-mixed-content.js
-
+echo "Running mixed content report"
 ./report-mixed-content.js ../_site/test/index.html
-
+echo "report finsihed."
 rm report-mixed-content.js
 cd ..
 rmdir files
