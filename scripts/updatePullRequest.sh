6246d4cf3f63f27bc688a0359c21eda30a2feb32
Glen Robson <glen.robson@gmail.com>
2018-09-13 02:39:06 +0100

    Working with cache

--- scripts/updatePullRequest.sh
+++ scripts/updatePullRequest.sh
@@ -2,5 +2,8 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     COMMENT="Test it at https://preview.iiif.io/root/$TRAVIS_BRANCH"
+    echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+else
+    echo "Pull request was false";
 fi
