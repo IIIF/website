572b0101895eeae9878385182fbeb53860916155
Glen Robson <glen.robson@gmail.com>
2018-09-13 15:28:50 +0100

    correcting if

--- scripts/updatePullRequest.sh
+++ scripts/updatePullRequest.sh
@@ -4,7 +4,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     URL="https://preview.iiif.io/root/$TRAVIS_PULL_REQUEST_BRANCH"
     COMMENT="Branch aviliable for testing at"
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/IIIF/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments |grep "$COMMENT";
-    if [ $? -eq = 1]; then
+    if [ $? -eq = 1 ]; then
         echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
         curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT $URL\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
     else
