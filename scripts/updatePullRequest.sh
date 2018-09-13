bad97bf013929bba4cf9bbd568a2d9cffdc0fd1f
Glen Robson <glen.robson@gmail.com>
2018-09-13 14:58:16 +0100

    Moving to source branch rather than desitnation.

--- scripts/updatePullRequest.sh
+++ scripts/updatePullRequest.sh
@@ -2,7 +2,7 @@
 
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     COMMENT="Test it at https://preview.iiif.io/root/$TRAVIS_BRANCH"
-    echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+    echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST_BRANCH}/comments"
     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
 else
     echo "Pull request was false";
