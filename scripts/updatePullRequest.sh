d95d3a2047f290a1288f5a8029b7f8a759f0963f
Glen Robson <glen.robson@gmail.com>
2018-09-13 15:40:05 +0100

    Adding debug message

--- scripts/updatePullRequest.sh
+++ scripts/updatePullRequest.sh
@@ -3,6 +3,7 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     URL="https://preview.iiif.io/root/$TRAVIS_PULL_REQUEST_BRANCH"
     COMMENT="Branch aviliable for testing at"
+    echo "Checking if https://api.github.com/repos/IIIF/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments already has a link to the preview site..."
     curl -H "Authorization: token $GITHUB_TOKEN" https://api.github.com/repos/IIIF/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments |grep "$COMMENT";
     if [ $? -eq 1 ]; then
         echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
