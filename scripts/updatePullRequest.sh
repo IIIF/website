fc8b9eba882bd1539618517d710600798701ae32
Glen Robson <glen.robson@gmail.com>
2018-09-13 15:21:35 +0100

    testing multiple commits to pull request

--- scripts/updatePullRequest.sh
+++ scripts/updatePullRequest.sh
@@ -4,6 +4,7 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ] ; then
     COMMENT="Branch aviliable for testing at https://preview.iiif.io/root/$TRAVIS_PULL_REQUEST_BRANCH"
     echo "Adding $COMMENT to issue https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
     curl -H "Authorization: token $GITHUB_TOKEN" -X POST -d "{\"body\": \"$COMMENT\"}" "https://api.github.com/repos/${TRAVIS_REPO_SLUG}/issues/${TRAVIS_PULL_REQUEST}/comments"
+    
 else
     echo "Pull request was false";
 fi
